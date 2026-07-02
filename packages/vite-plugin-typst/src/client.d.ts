078bb21fc481cf5dd80405af4b27fbb14b3d37ab
will <will.bradshaw50@gmail.com>
2026-07-02 10:34:08 +1000

    feat(engine-wasm): add glue export

---
+++ packages/vite-plugin-typst/src/client.d.ts
@@ -0,0 +1,11 @@
+declare module "*.typ" {
+  import type { TypstCompiledModule } from "@typst-wasm/vite-plugin-typst";
+
+  export const html: TypstCompiledModule["html"];
+  export const metadata: TypstCompiledModule["metadata"];
+  export const diagnostics: TypstCompiledModule["diagnostics"];
+  export const dependencies: TypstCompiledModule["dependencies"];
+
+  const document: TypstCompiledModule;
+  export default document;
+}
