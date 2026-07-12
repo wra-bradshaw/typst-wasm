494708517d5f8e8c5405bcc1b53fb4291e729bc9
will <will.bradshaw50@gmail.com>
2026-07-13 00:17:58 +1000

    ci: deno doesnt have timeout

--- packages/typst-wasm/tests/integration/adapters/deno.ts
+++ packages/typst-wasm/tests/integration/adapters/deno.ts
@@ -5,7 +5,6 @@ import { makeDenoContext } from "../contexts/deno.ts";
 
 Deno.test({
   name: "deno:worker canonical integration suite",
-  timeout: 120_000,
   async fn() {
     assertSuitePassed(
       await runSuite(await makeDenoContext("worker"), canonicalCases),
