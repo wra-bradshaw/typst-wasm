7162f5814b2d3033fdaa658f87343a6aedc729ce
will <will.bradshaw50@gmail.com>
2026-07-16 22:22:41 +1000

    refactor(typst-wasm): inline public API exports

--- packages/typst-wasm/src/index.ts
+++ packages/typst-wasm/src/index.ts
@@ -5,4 +5,43 @@ export {
   supportsWorkerBackend,
 } from "./backends/index";
 export type { BackendKind, BackendSelection } from "./backends/index";
-export * from "./public-api";
+
+export type {
+  AnyCompileResult,
+  BundleCompileResult,
+  CompileFormat,
+  CompileInputs,
+  CompileOptions,
+  CompileResult,
+  Diagnostic,
+  DocumentMetadata,
+  FetchRequest,
+  FontInput,
+  FetchedFile,
+  FileKind,
+  HtmlCompileResult,
+  LoadedFile,
+  PdfCompileResult,
+  PdfStandard,
+  PngCompileResult,
+  SvgCompileResult,
+  TypstCompiler,
+  TypstCompilerOptions,
+  TypstFileLoader,
+  TypstLogLevel,
+  TypstLogger,
+} from "./compiler/types";
+export type { CoreModuleName, CoreModules } from "./engine/types";
+export type { WorkerHost } from "./worker/host";
+export type { PackageCache } from "./files";
+export {
+  CompileError,
+  CompilerDisposedError,
+  CompilerNotInitializedError,
+  FetchError,
+  FileNotFoundError,
+  PackageFetchError,
+  PackageParseError,
+  TypstError,
+  WorkerError,
+} from "./errors";
